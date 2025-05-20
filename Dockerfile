c626713cb7df523ff24fd232b37f732e624e81e8
Chief-spartan-117 <shresthasagar861@gmail.com>
2025-05-20 16:47:03 +0545

    fix: host configuration of application and database in docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "app:app"]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8000", "server:app"]
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
+import os
+import logging
 from flask import Flask
 from app import create_app
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
-
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    debug_mode = os.getenv("FLASK_ENV")
+    # debug_mode = os.getenv("FLASK_ENV")
+    try:
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    except Exception as e:
+        logging.error(f"Failed to start server: {e}", exc_info=True)
+        raise
