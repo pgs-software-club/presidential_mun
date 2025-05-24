10bdd25a3024deecf021230f2eaaf6f04dbd0da1
Chief-spartan-117 <shresthasagar861@gmail.com>
2025-05-24 10:34:44 +0545

    chore: entrypoint.sh removed some commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 flask db init
-flask db migrate -m "Intial Migration"
-flask db upgrade
+# flask db migrate -m "Intial Migration"
+# flask db upgrade
 exec gunicorn -w 4 -b 0.0.0.0:8000 server:app
